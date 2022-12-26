df4b248fb762073f94e3d89f27c685164294c5c4
mlorber <87738509+mlorberdev@users.noreply.github.com>
2022-12-26 11:40:06 -0500

    added color palette, lock, and info

--- assets/scripts/genart.js
+++ assets/scripts/genart.js
@@ -1,3 +1,4 @@
+import { palettes } from "./palettes.js";
 import { disko } from "./disko.js";
 import { nooks } from "./nooks.js";
 import { entropy } from "./entropy.js";
@@ -11,20 +12,44 @@ import { boxy } from "./boxy.js";
 import { grid } from "./grid.js";
 
 !(function () {
+
+	const writeHues = () => { for (let i = 0; i < 5; i++) document.getElementById(`hue${i}`).style.backgroundColor = hues[i]; };
+	let lock = document.getElementById("lock");
+	let hues;
+
+	function newHues() {
+		if (lock.checked) return;
+		hues = palettes[Math.floor(Math.random() * palettes.length)];
+		writeHues();
+	} newHues();
+
+	function newBgHue() {
+		let bg = this.style.backgroundColor;
+		hues = [bg];
+		let swatches = document.querySelectorAll(".hue");
+		for (let i = 0; i < 4; i++) if (swatches[i].style.backgroundColor !== bg) hues.push(swatches[i].style.backgroundColor);
+		writeHues();
+	}
+
+	document.querySelectorAll(".hue").forEach(hue => hue.addEventListener("click", newBgHue));
+
 	document.getElementById("chooseArt").querySelectorAll(":scope > button").forEach(e => e.addEventListener("click", function () {
+
+		if (lock.checked === false) newHues();
 
 		switch (this.innerText.toLowerCase()) {
 			case ("disko"): disko(); break;
 			case ("nooks"): nooks(); break;
-			case ("entropy"): entropy(); break;
-			case ("lines"): lines(); break;
-			case ("pack"): pack(); break;
-			case ("neighbors"): neighbors(); break;
-			case ("paths"): paths(); break;
-			case ("molnar"): molnar(); break;
-			case ("bebebe"): bebebe(); break;
-			case ("boxy"): boxy(); break;
-			case ("grid"): grid(); break;
+			case ("entropy"): entropy(hues); break;
+			case ("lines"): lines(hues); break;
+			case ("pack"): pack(hues); break;
+			case ("neighbors"): neighbors(hues); break;
+			case ("paths"): paths(hues); break;
+			case ("molnar"): molnar(hues); break;
+			case ("bebebe"): bebebe(hues); break;
+			case ("boxy"): boxy(hues); break;
+			case ("grid"): grid(hues); break;
 		}
+
 	}));
 })();
